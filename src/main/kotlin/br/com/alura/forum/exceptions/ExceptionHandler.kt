fa1ba1eaff987f25a313bb3b4debd6abf489e95f
Ruan Failache <ruanfailache@gmail.com>
2022-10-24 11:41:24 -0300

    feat: implements more exceptions

--- src/main/kotlin/br/com/alura/forum/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exceptions/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exceptions
 
 import br.com.alura.forum.dto.output.ErrorOutput
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -11,13 +12,45 @@ import javax.servlet.http.HttpServletRequest
 class ExceptionHandler {
     @ExceptionHandler(NotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFound(
+    fun handleNotFoundError(
         exception: NotFoundException,
         request: HttpServletRequest,
     ): ErrorOutput {
         return ErrorOutput(
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath,
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest,
+    ): ErrorOutput {
+        val errorMessage = HashMap<String, String>()
+        exception.bindingResult.fieldErrors.forEach { error ->
+            errorMessage[error.field] = error.defaultMessage.toString()
+        }
+        return ErrorOutput(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath,
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServerError(
+        exception: Exception,
+        request: HttpServletRequest,
+    ): ErrorOutput {
+        return ErrorOutput(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exception.message,
             path = request.servletPath,
         )
